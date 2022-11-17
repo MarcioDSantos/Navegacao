350b50f543270de02ad183826c1499751397ebea
Marcio Santos <marcioluisborgesds@gmail.com>
2022-11-17 00:11:36 -0300

    Estruturação projeto Navegação

---
+++ src/components/Content.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Content from './Content.css';
+
+const Content = props => (
+    <main className="Content">
+        Componente Content
+    </main>
+)
+
+export default Content
---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = props => (
+    <aside className="Home">
+        <h1>CInicio</h1>
+        <p>Bem Vindos!</p>
+    </aside>
+)
+
+export default Home
